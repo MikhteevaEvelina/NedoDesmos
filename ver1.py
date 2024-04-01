32ad091d1fd50f663fc841abd48604320fe619dc
MikhteevaEvelina <evelinami@mail.ru>
2024-04-01 23:49:32 +0300

    Новый формат ввода

--- ver1.py
+++ ver1.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 from funk import solving_eq
+import re
+
+
+def parse_eq(eq):
+    try:
+        m = re.match(
+            r"\s*([+-]?\d*\.?\d*)\s*x\^2\s*([+-]?\d*\.?\d*)\s*x\s*([+-]?\d*\.?\d*)\s*$",
+            eq,
+        )
+        if m:
+            a = float(m.group(1).replace(" ", "") or "1")
+            b = float(m.group(2).replace(" ", "") or "1")
+            c = float(m.group(3).replace(" ", "") or "0")
+            return [a, b, c]
+
+        raise ValueError("Неверный формат ввода")
+
+    except ValueError:
+        messagebox.showerror("Error", "Неверный формат ввода")
 
 
 def main():
@@ -12,31 +31,29 @@ def main():
     label_res_x_2.config(text="")
     label_res.config(text="")
 
+    eq = entry_eq.get()
+    abc = parse_eq(eq)
+    a = abc[0]
+    b = abc[1]
+    c = abc[2]
+    answer = solving_eq(a, b, c)
     try:
-        a = float(entry_a.get() or 0)
-        b = float(entry_b.get() or 0)
-        c = float(entry_c.get() or 0)
-        answer = solving_eq(a, b, c)
-        try:
-            canvas_widget.destroy()
-        except:
-            pass
-        visual_eq(a, b, c)
+        canvas_widget.destroy()
+    except:
+        pass
+    visual_eq(a, b, c)
 
-        if len(answer) == 1:
-            label_res.config(text=answer[0])
+    if len(answer) == 1:
+        label_res.config(text=answer[0])
 
-        elif len(answer) == 2:
-            label_res.config(text=answer[0])
-            label_res_x_1.config(text=answer[1])
+    elif len(answer) == 2:
+        label_res.config(text=answer[0])
+        label_res_x_1.config(text=answer[1])
 
-        elif len(answer) == 3:
-            label_res.config(text=answer[0])
-            label_res_x_1.config(text=answer[1])
-            label_res_x_2.config(text=answer[2])
-
-    except ValueError:
-        messagebox.showerror("Error", "Неверный формат ввода")
+    elif len(answer) == 3:
+        label_res.config(text=answer[0])
+        label_res_x_1.config(text=answer[1])
+        label_res_x_2.config(text=answer[2])
 
 
 def visual_eq(a, b, c):
@@ -58,9 +75,8 @@ def visual_eq(a, b, c):
 
 def clean_window():
     global canvas_widget
-    entry_a.delete(0, tk.END)
-    entry_b.delete(0, tk.END)
-    entry_c.delete(0, tk.END)
+    entry_eq.delete(0, tk.END)
+    entry_eq.insert(0, "0x^2+0x+0")
     label_res_x_1.config(text="")
     label_res_x_2.config(text="")
     label_res.config(text="")
@@ -73,34 +89,24 @@ def clean_window():
 window = tk.Tk()
 window.title("Решение квадратных уравнений")
 
-label_1 = tk.Label(window, text="Введите коэффициенты квадратного уравнения:")
+label_1 = tk.Label(window, text="Введите квадратное уравнение:")
 label_1.grid(row=0, column=0, columnspan=7, padx=5, pady=5)
-entry_a = tk.Entry(window, width=5)
-entry_a.grid(row=1, column=0, padx=5, pady=5)
-entry_a.insert(0, "0")
-label_x2 = tk.Label(window, text="x² + ")
-label_x2.grid(row=1, column=1, padx=5, pady=5)
-entry_b = tk.Entry(window, width=5)
-entry_b.grid(row=1, column=2, padx=5, pady=5)
-entry_b.insert(0, "0")
-label_x = tk.Label(window, text="x + ")
-label_x.grid(row=1, column=3, padx=5, pady=5)
-entry_c = tk.Entry(window, width=5)
-entry_c.insert(0, "0")
-entry_c.grid(row=1, column=4, padx=5, pady=5)
+entry_eq = tk.Entry(window, width=30)
+entry_eq.grid(row=2, column=0, columnspan=5, padx=5, pady=5)
+entry_eq.insert(0, "0x^2+0x+0")
 label_0 = tk.Label(window, text="= 0")
-label_0.grid(row=1, column=5, padx=5, pady=5)
+label_0.grid(row=2, column=6, padx=5, pady=5)
 
 label_res = tk.Label(window)
-label_res.grid(row=3, column=0, columnspan=7, padx=5, pady=5)
+label_res.grid(row=5, column=0, columnspan=7, padx=5, pady=5)
 label_res_x_1 = tk.Label(window)
-label_res_x_1.grid(row=4, column=0, columnspan=7, padx=5, pady=5)
+label_res_x_1.grid(row=6, column=0, columnspan=7, padx=5, pady=5)
 label_res_x_2 = tk.Label(window)
-label_res_x_2.grid(row=5, column=0, columnspan=7, padx=5, pady=5)
+label_res_x_2.grid(row=7, column=0, columnspan=7, padx=5, pady=5)
 
 solving = tk.Button(window, text="Решить", command=main)
-solving.grid(row=2, column=0, padx=5, pady=5)
+solving.grid(row=4, column=0, padx=5, pady=5)
 clear = tk.Button(window, text="Очистить", command=clean_window)
-clear.grid(row=2, column=1, padx=5, pady=5)
+clear.grid(row=4, column=1, padx=5, pady=5)
 
 window.mainloop()
